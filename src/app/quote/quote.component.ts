506cd76880565ce686b2b7af13c64dc5dfe1d0c3
sophieO1970 <sophieO1970@gmail.com>
2020-09-27 02:38:49 +0300

    add a custom directive to highlight the quote that has the highest upvotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -38,19 +38,19 @@ export class QuoteComponent implements OnInit {
     }
   }
 
-  // highestUpVote() {
-  //   let highestUpVoteQuote = new Quote(0, '', '', '', new Date());
-  //   for (let i = 0; i < this.quotes.length; i++) {
-  //     if (this.quotes[i].upvote > highestUpVoteQuote.upvote) {
-  //       highestUpVoteQuote = this.quotes[i];
-  //     }
-  //   }
-  //   if (highestUpVoteQuote.upvote > 0) {
-  //     return highestUpVoteQuote;
-  //   } else {
-  //     return;
-  //   }
-  // }
+  highestUpVote() {
+    let highestUpVoteQuote = new Quote(0, '', '', '', new Date());
+    for (let i = 0; i < this.quotes.length; i++) {
+      if (this.quotes[i].upvote > highestUpVoteQuote.upvote) {
+        highestUpVoteQuote = this.quotes[i];
+      }
+    }
+    if (highestUpVoteQuote.upvote > 0) {
+      return highestUpVoteQuote;
+    } else {
+      return;
+    }
+  }
 
   // addNewQuote(newQuote: Quote) {
   //   newQuote.id = this.quotes.length + 1;
