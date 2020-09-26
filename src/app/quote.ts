9211589dc1ef3d1e8cdf493805697e84749bc021
sophieO1970 <sophieO1970@gmail.com>
2020-09-26 15:22:29 +0300

    add a class constructor in goal.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,4 +4,12 @@ export class Quote {
     dateAdded: Date;
     upvote: number;
     downvote: number;
+
+    constructor(text:string, author:string){
+        this.text = text;
+        this.author =author;
+        this.dateAdded = new Date();
+        this.upvote = 0;
+        this.downvote =0;
+    }
 }
