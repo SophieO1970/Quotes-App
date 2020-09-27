c2de0a3506395158a5810bdb2a837c2e3d4526af
sophieO1970 <sophieO1970@gmail.com>
2020-09-27 04:13:07 +0300

    add the add quote which includes an empty array with new date

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote: Quote = new Quote(0, '', '', '', new Date());
 
-  constructor() { }
+  @Output() createQuote = new EventEmitter<Quote>();
 
-  ngOnInit(): void {
+  addQuote() {
+    this.createQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0, "", "", "", new Date());
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
